660c4272834e60bf769a1e93e38f2adc1c65a93b
michellepuyat <puy20001@byui.edu>
2023-02-16 00:26:22 +0800

    push

--- Problem1/js/myscript.js
+++ Problem1/js/myscript.js
@@ -1,9 +1,17 @@
 function percentage(){
-    //INPUT - number of classes held; number of classess attended
     let classHeld = parseFloat(document.getElementById('classHeld').value);
     let classAttended = parseFloat(document.getElementById('classAttended').value);
-    //PROCESS - divide number of classes attended by the number of classes held
     let percentageOfClassesAttended = classAttended / classHeld * 100;
-    //OUTPUT - weight converted in kg
+    let remarks;
+
     document.getElementById('output').innerHTML = percentageOfClassesAttended.toFixed(1) + "%";
+
+    if (percentageOfClassesAttended >= 75) {
+        remarks = "You can take the exam 👌"
+    }
+    else {
+        remarks = "You are not allowed to take the exam 😥"
+    }
+    //OUTPUT - the necessary remarks for correct and incorrect answer
+    document.getElementById('remarks').innerHTML = remarks;
 }
