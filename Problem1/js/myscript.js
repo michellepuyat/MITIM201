bdd26be7200d84920725e6b60cc62b1326ea2058
michellepuyat <puy20001@byui.edu>
2023-02-16 00:18:45 +0800

    push

---
+++ Problem1/js/myscript.js
@@ -0,0 +1,9 @@
+function percentage(){
+    //INPUT - number of classes held; number of classess attended
+    let classHeld = parseFloat(document.getElementById('classHeld').value);
+    let classAttended = parseFloat(document.getElementById('classAttended').value);
+    //PROCESS - divide number of classes attended by the number of classes held
+    let percentageOfClassesAttended = classAttended / classHeld;
+    //OUTPUT - weight converted in kg
+    document.getElementById('output').innerHTML = kg.toFixed(1) + "%";
+}
