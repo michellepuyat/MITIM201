bff10f6400b83b9d8c294a423891fbc4d8bf23a3
michellepuyat <puy20001@byui.edu>
2023-02-16 02:35:55 +0800

    push

--- Problem2/js/myscript.js
+++ Problem2/js/myscript.js
@@ -1,17 +1,46 @@
-function percentage(){
-    let classHeld = parseFloat(document.getElementById('classHeld').value);
-    let classAttended = parseFloat(document.getElementById('classAttended').value);
-    let percentageOfClassesAttended = classAttended / classHeld * 100;
-    let remarks;
+document.getElementById("currency").addEventListener("change", changeLabel)
+document.getElementById("currency1").defaultValue = 1;
 
-    document.getElementById('output').innerHTML = percentageOfClassesAttended.toFixed(1) + "%";
+function changeLabel(){
+    let currencyChosen = document.getElementById('currency').value;
+    
+    switch (currencyChosen) {
+        case 'us':
+            document.getElementById('from').innerHTML = "US Dollar: ";
+            break;
+        case 'hk':
+            document.getElementById('from').innerHTML = "HK Dollar: ";
+            break;
+        case 'au':
+            document.getElementById('from').innerHTML = "AU Dollar: ";
+            break;
+        case 'nz':
+            document.getElementById('from').innerHTML = "NZ Dollar: ";
+            break;
+        case 'jp':
+            document.getElementById('from').innerHTML = "Japan Yen: ";
+            break;
+        case 'sa':
+            document.getElementById('from').innerHTML = "SA Riad (Dinnar): ";
+            break;
+        case 'uk':
+            document.getElementById('from').innerHTML = "UK Pound: ";
+            break;
+    }
+}
 
-    if (percentageOfClassesAttended >= 75) {
-        remarks = "You can take the exam 👌"
-    }
-    else {
-        remarks = "You are not allowed to take the exam 😥"
-    }
-    //OUTPUT - the necessary remarks for correct and incorrect answer
-    document.getElementById('remarks').innerHTML = remarks;
+function convert(){
+    let exchangeRate = parseFloat(document.getElementById('exchangeRate').value);
+    let from = parseFloat(document.getElementById('currency1').value);
+    let to = exchangeRate * from;
+
+    document.getElementById('currency2').value = to.toFixed(1);
+}
+
+function convert2(){
+    let exchangeRate = parseFloat(document.getElementById('exchangeRate').value);
+    let from = parseFloat(document.getElementById('currency2').value);
+    let to = from / exchangeRate;
+
+    document.getElementById('currency1').value = to.toFixed(1);
 }
